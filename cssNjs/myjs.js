bd2851cded50f8f2c6a7075a0ec44b26aeb155bc
xaviorhsu <adnil.hsu@gmail.com>
2022-05-26 17:03:24 +0800

    5/26

--- cssNjs/myjs.js
+++ cssNjs/myjs.js
@@ -1,10 +1,10 @@
 $(document).ready(function()  {
-    var http_ip=(location.hostname=="websrv.local.tw")?"http://websrv.local.tw/":"https://xaviorhsu.github.io/";
+    var http_ip=(location.hostname=="websrv.local.tw")?"http://websrv.local.tw/":"https://"+location.hostname+"/";
     if (location.hostname=="websrv.local.tw"){
 		$("#dj2").attr("href",http_ip+"TTBus/index.php");
 		$("#dj5").attr("href",http_ip+"mydjweb/b20");
 		}
-	$('#a1,#a2,#a3,#a4,#a5,#a6,#a7').css('cursor','pointer').click(function(){
+	$('#a1,#a2,#a3,#a4,#a5,#a6,#a7,#a8,#a9').css('cursor','pointer').click(function(){
     var i=($(this).attr("id").substring(1));    
     var w_url = http_ip+"webportal/wp.html?sw="+i; window.open(w_url,"dj",);
     });
--- cssNjs/port.js
+++ cssNjs/port.js
@@ -1,26 +1,28 @@
     var HTTP_IP=(location.hostname=="websrv.local.tw")?"http://websrv.local.tw/":"https://"+location.hostname+"/";
 
-    var website =["","forevers","cu4103","hsusF","webapp","SR2FB","All4u","ue1rent"];
-    var pagename=['','安久號烏魚子專門店','台南市開山儲蓄互助社-Welcome首頁','徐氏歷代子孫在台族譜','物聯網Webapp_Project','物聯網SendRecv_Firebase case','All4u-080共生小棧','招租㊣紅瓦厝'];
+    var website =["","hsuvm6","forevers","hsusF","webapp","SR2FB","All4u","cu4103","ue1rent","tncath"];
+    var pagename=['','職訓班實習實作網誌','安久號烏魚子專門店','徐氏歷代子孫在台族譜','物聯網Webapp_Project','物聯網SendRecv_Firebase case','All4u-080共生小棧','台南市開山儲蓄互助社-Welcome首頁','招租㊣紅瓦厝','天主教台南主教座堂'];
     var idx=(location.hostname=="websrv.local.tw")?"0":"1";
 	var paValue=getUrlVars("sw");
-    idx=(paValue)?paValue:0;
+    idx=(paValue)?paValue:0; 
     var pv=document.getElementById('pn1');
     pv.innerHTML=(pagename[idx]==''||pagename[idx]==undefined)?"The URL not found ! (Error 404)":pagename[idx];    
     if(website[idx]!=""&&website[idx]!=undefined) {
       switch(idx)  {
           case "0" : { alert("<== Pause 0st ==>"); } break;
-          case "1" : { if(location.hostname=="websrv.local.tw")
-							setTimeout("top.location='https://www.forevers.com.tw';",3000);
-					   else setTimeout("top.location='"+HTTP_IP+website[idx]+"/';",3000); } break;
-          case "2" : { setTimeout("top.location='"+HTTP_IP+website[idx]+"/';",3000); } break;
+          case "1" : { setTimeout("top.location='https://hsuvm6.blogspot.com/2015/06/';",3000); } break;
+          case "2" : { setTimeout("top.location='"+HTTP_IP+website[idx]+"/';",3000);
+					 /*if(location.hostname!="websrv.local.tw")
+							setTimeout("top.location='"+HTTP_IP+website[idx]+"/';",3000);
+					   else setTimeout("top.location='https://www.forevers.com.tw';",3000);*/ } break;
           case "3" : { setTimeout("top.location='"+HTTP_IP+website[idx]+"/hsus_Family.html';",3000); } break;
           case "4" : { setTimeout("top.location='"+HTTP_IP+website[idx]+"/00webappproject1.html';",3000); } break;
           case "5" : { setTimeout("top.location='"+HTTP_IP+website[idx]+"/Send_Recv.html';",3000); } break;
-          case "6" : { if (location.hostname=="websrv.local.tw") { setTimeout("top.location='"+HTTP_IP+website[idx]+"/';",3000); }
-                       else  { setTimeout("top.location='https://www.all4u.me';",3000); } } break;
+          case "6" : { setTimeout("top.location='"+HTTP_IP+website[idx]+"/';",3000); } break;
           case "7" : { setTimeout("top.location='"+HTTP_IP+website[idx]+"/';",3000); } break;
-          case "8" : { alert("<== Pause 7st ==>"); } break;
+		  case "8" : { setTimeout("top.location='"+HTTP_IP+website[idx]+"/';",3000); } break;
+		  case "9" : { setTimeout("top.location='"+HTTP_IP+website[idx]+"/';",3000); } break;
+          case "A" : { alert("<== Pause Ast ==>"); } break;
                    }
       /*setTimeout("top.location='"+HTTP_IP+website[idx]+"/';",3000);*/
          }
